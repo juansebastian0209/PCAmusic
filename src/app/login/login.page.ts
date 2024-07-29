9c2ff256403665c130e9fbbc1fe4de9d03e9f682
Juan Delgado <juanse140614@gmail.com>
2024-07-29 16:45:51 -0500

    Form-Validation

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -13,6 +13,16 @@ import {
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  validation_messages = {
+    email: [
+      { type: 'required', message: 'El email es obligatorio' },
+      { type: 'pattern', message: 'Email invalido' },
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es obligatoria' },
+      { type: 'pattern', message: 'Contraseña invalida' },
+    ],
+  };
   constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
       email: new FormControl(
